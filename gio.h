1c02b104bcc5693ce4b35dc520bb0d6a23448919
Tina Turner <tinaturner@whatslovegottodo.withit>
2021-08-28 16:36:26 +0000

    gio

--- gio.h
+++ gio.h
@@ -73,6 +73,13 @@ static inline void * _kk_gio_xalloc(void *ptr, size_t sz)
 # define KK_GIO_TRACE(...)
 #endif
 
+/* We use void so we can pass a pointer to the struct gio 
+ * as well as any of the derived types.
+ *
+ * TODO: If you want to be extra type safe maybe wrap it in
+ * a struct but oof really. */ 
+typedef void gio_t;
+
 enum {
   GIO_INVALID,
   GIO_FILE,
@@ -83,6 +90,30 @@ enum {
 struct gio {
   uint8_t type;
   uint8_t flags;
+};
+
+enum {
+  GIO_MEM_ALLOC = 0x1,
+
+  /* This flag will try to automatically reallocate the underlying buffer
+   * when we run out of space to write (or allocate a new one if buffer is
+   * NULL).
+   *
+   * If you pass a buffer into an init function and this flag is set, the buffer
+   * must be reallocable and freeable with the KK_GIO_XALLOC function.
+   */
+  GIO_MEM_AUTOGROW = GIO_MEM_ALLOC | 0x2,
+
+  /* When you print string to a buffer in multiple calls you need to 
+   * seek back one after each one to overwrite the terminating '/0'.
+   *
+   * This flag automatically checks if the last character is '/0' and 
+   * seeks back one on prints (but not on writes). See gio_nprintf().
+   *
+   * Note that because it checks and not just seek back blindly, 
+   * code that manually seeks back will continue to work.
+   */
+  GIO_MEM_STRING_AUTOCONTINUE = 0x4, 
 };
 
 struct gio_mem {
@@ -98,14 +129,26 @@ struct gio_file {
   int fd;
 };
 
+enum {
+  GIO_CTL_CLOSE, 
+  GIO_CTL_SYNC,
+  /* define your custom ones starting here */
+  GIO_CTL_USER, 
+};
+
 struct gio_ops {
   struct gio gio;
   void *data;
   ssize_t (*write)(struct gio_ops *, const void *, size_t);
   ssize_t (*read)(struct gio_ops *, void *, size_t);
   off_t (*seek)(struct gio_ops *, off_t, int);
+  /* those two could be one multiplexed call, 
+   * it ain't really worth it though, is it? */
   int (*sync)(struct gio_ops *);
   int (*close)(struct gio_ops *);
+  /* This should work as sync, close and user-defined
+   * custom operations. */
+  int (*ctl)(struct gio_ops *, int op, void *user);
 };
 
 #define KK_GIO_API
@@ -125,7 +168,10 @@ KK_GIO_API
 int gio_mem_init(struct gio_mem *gio, void *buf, size_t sz);
 
 KK_GIO_API
-struct gio_mem gio_mem_new(void *buf, size_t sz);
+struct gio_mem gio_mem_new(void *buf, size_t sz, uint8_t flags);
+
+KK_GIO_API
+struct gio_mem gio_mem_new_alloc(size_t sz);
 
 /* file pointer api */
 
@@ -173,25 +219,62 @@ int gio_list_add(struct gio_ops *list, struct gio *gio);
 /* generic api */
 
 KK_GIO_API
-ssize_t gio_write(void *_gio, const void *buf, size_t sz);
+ssize_t gio_write(gio_t *_gio, const void *buf, size_t sz);
 
 KK_GIO_API
-ssize_t gio_read(void *_gio, void *buf, size_t sz);
+ssize_t gio_read(gio_t *_gio, void *buf, size_t sz);
 
 KK_GIO_API
-off_t gio_seek(void *_gio, off_t off, int whence);
+off_t gio_seek(gio_t *_gio, off_t off, int whence);
 
 KK_GIO_API
-int gio_printf(void *_gio, const char *fmt, ...);
+off_t gio_pipe(gio_t *src, gio_t *dst);
+
+//KK_GIO_API
+//int gio_printf(gio_t *_gio, const char *fmt, ...);
+
+/* NOTE:
+ * You know how you can only call v*print family of functions once as the 
+ * va_list passed is undefined after a call? Well, now you know. 
+ * This is unfortunate if you need to buffer the output and need to first 
+ * query for the buffer size with vsprintf. Because of that you cannot have 
+ * multiple wrapper functions around printf.
+ */
 
 KK_GIO_API
-int gio_vprintf(void *_gio, const char *fmt, va_list args);
+int gio_nprintf(gio_t *_gio, size_t maxn, const char *fmt, ...);
+
+#define gio_printf(GIO,...) gio_nprintf(GIO, SIZE_MAX, __VA_ARGS__)
+
+
+static inline
+int gio_rep(gio_t *_gio, int n, const char *str, int len)
+{
+  int written = 0;
+
+  for(int i = 0; i < n; ++i)
+    written += gio_write(_gio, str, len);
+
+  return written;
+}
+
+int gio_ctl(gio_t *_gio, int op, void *user);
 
 KK_GIO_API
-int gio_sync(void *_gio);
+static inline
+int gio_sync(gio_t *_gio)
+{
+  return gio_ctl(_gio, GIO_CTL_SYNC, NULL);
+}
 
 KK_GIO_API
-int gio_close(void *_gio);
+static inline
+int gio_close(gio_t *_gio)
+{
+  return gio_ctl(_gio, GIO_CTL_CLOSE, NULL);
+}
+
+
 
 #endif /* _KK_GIO_H_ */
 
@@ -204,6 +287,18 @@ int gio_close(void *_gio);
  */
 
 #ifdef KK_GIO_IMPL
+
+static inline void * _gio_xalloc(void *ptr, size_t sz)
+{
+  void * ret = KK_GIO_XALLOC(ptr, sz);
+#ifdef KK_GIO_ENABLE_TRACING
+  fprintf(stderr,
+      "%s(): ptr=%p, sz=%zu -> %p\n", __func__, ptr, sz, ret);
+  fflush(stderr);
+#endif
+  return ret;
+}
+
 
 /* in memory api */
 
@@ -223,12 +318,23 @@ int gio_mem_init(struct gio_mem *gio, void *buf, size_t sz)
 }
 
 KK_GIO_API
-struct gio_mem gio_mem_new(void *buf, size_t sz)
+struct gio_mem gio_mem_new(void *buf, size_t sz, uint8_t flags)
 {
   struct gio_mem gio_mem = {};
   int rc = gio_mem_init(&gio_mem, buf, sz);
   assert(rc == 0);
+  gio_mem.gio.flags = flags;
   return gio_mem;
+}
+
+KK_GIO_API
+struct gio_mem gio_mem_new_alloc(size_t sz)
+{
+  void *buf = _gio_xalloc(NULL, sz);
+  assert(buf);
+  struct gio_mem ret = gio_mem_new(buf, sz, 0);
+  ret.gio.flags |= GIO_MEM_ALLOC;
+  return ret;
 }
 
 /* file pointer api */
@@ -287,16 +393,45 @@ struct gio_file gio_file_new_fd(int fd)
 
 /* generic api */
 
+size_t _gio_mem_left(struct gio_mem *gio) 
+{
+  KK_GIO_ASSERT(gio->sz >= gio->off);
+  return gio->sz - gio->off;
+}
+
+int _gio_mem_maybe_grow(struct gio_mem *gio, size_t sz)
+{
+  if(gio->gio.flags & GIO_MEM_ALLOC && gio->gio.flags & GIO_MEM_AUTOGROW) {
+    if(sz > _gio_mem_left(gio)) {
+      size_t new_sz = GIO_MAX(gio->sz * 2, gio->sz + sz);
+      void *new_buf = _gio_xalloc(gio->buf, new_sz);
+      if(!new_buf)
+        return -1;
+
+      gio->buf = new_buf;
+      gio->sz = new_sz;
+    }
+  }
+  return 0;
+}
+
 KK_GIO_API
-ssize_t gio_write(void *_gio, const void *buf, size_t sz)
+ssize_t gio_write(gio_t *_gio, const void *buf, size_t sz)
 {
   KK_GIO_TRACE(
       "%s(): gio=%p { .type=%d }, buf=%p, sz=%zu\n", 
       __func__, _gio, ((struct gio*)_gio)->type, buf, sz);
 
+  if(!_gio)
+    return sz;
+
   switch(((struct gio*)_gio)->type) {
   case GIO_MEM: {
     struct gio_mem *gio = (struct gio_mem*)_gio;
+
+    if(_gio_mem_maybe_grow(gio, sz))
+      return -1;
+
     assert(gio->sz >= gio->off);
     sz = GIO_MIN(sz, gio->sz - gio->off);
     memcpy(gio->buf + gio->off, buf, sz);
@@ -324,7 +459,7 @@ ssize_t gio_write(void *_gio, const void *buf, size_t sz)
 }
 
 KK_GIO_API
-ssize_t gio_read(void *_gio, void *buf, size_t sz)
+ssize_t gio_read(gio_t *_gio, void *buf, size_t sz)
 {
   KK_GIO_TRACE(
       "%s(): gio=%p { .type=%d }, buf=%p, sz=%zu\n", 
@@ -360,11 +495,9 @@ ssize_t gio_read(void *_gio, void *buf, size_t sz)
 }
 
 KK_GIO_API
-off_t gio_seek(void *_gio, off_t off, int whence)
+off_t gio_seek(gio_t *_gio, off_t off, int whence)
 {
-  KK_GIO_TRACE(
-      "%s(): gio=%p { .type=%d }, off=%d, whence=%d\n", 
-      __func__, _gio, ((struct gio*)_gio)->type, off, whence);
+  int ret = -1;
 
   switch(((struct gio*)_gio)->type) {
   case GIO_MEM: {
@@ -372,181 +505,278 @@ off_t gio_seek(void *_gio, off_t off, int whence)
     switch(whence) {
     case SEEK_SET:
       if(off < 0 || off > gio->sz)
-        return -1;
+        goto out;
 
       gio->off = off;
       break;
 
     case SEEK_CUR:
       if(off >= 0) {
+        /* TODO: in autogrow mode don't fail, just allocate enough space */
         if(off > gio->sz - gio->off)
-          return -1;
+          goto out;
 
       } else {
         if(-off > gio->off)
-          return -1;
+          goto out;
       }
       gio->off += off;
       break;
 
     case SEEK_END:
       if(off > 0 || -off > gio->sz)
-        return -1;
+        goto out;
 
       gio->off = gio->sz + off;
       break;
 
     default:
-      return -1;
+      goto out;
     }
-    return gio->off;
+    ret = gio->off;
+    break;
   }
   case GIO_FILE: {
     struct gio_file *gio = (struct gio_file*)_gio;
     if(gio->fp)
-      return fseek(gio->fp, off, whence);
+      ret = fseek(gio->fp, off, whence);
     else
-      return lseek(gio->fd, off, whence);
+      ret = lseek(gio->fd, off, whence);
+
+    goto out;
   }
   case GIO_OPS: {
     struct gio_ops *gio = (struct gio_ops*)_gio;
-    if(gio->seek)
-      return gio->seek(gio, off, whence);
-    else
-      return -1;
-  }
-  defualt:
-    KK_GIO_UNREACHABLE();
-  }
-  return -1;
-}
+    if(gio->seek) 
+      ret = gio->seek(gio, off, whence);
 
-static inline
-void * _gio_alloc_snprintf(int *ret_sz, const char *fmt, va_list args) 
-{
-  int len = KK_GIO_PRINTF_BUFSZ;
-  char *buf;
-
-  if(!(buf = KK_GIO_XALLOC(NULL, KK_GIO_PRINTF_BUFSZ)))
-    return NULL;
-
-  if((len = vsnprintf(buf, KK_GIO_PRINTF_BUFSZ, fmt, args)) < 0)
-    return NULL;
-
-  if(len > KK_GIO_PRINTF_BUFSZ) {
-    if(!(buf = KK_GIO_XALLOC(NULL, len)))
-      return NULL;
-
-    if((len = vsnprintf(buf, len, fmt, args)) < 0)
-      return NULL;
-  }
-  if(ret_sz)
-    *ret_sz = len;
-
-  return buf;
-}
-
-KK_GIO_API
-int gio_vprintf(void *_gio, const char *fmt, va_list args) 
-{
-  int ret = -1;
-
-  switch(((struct gio*)_gio)->type) { 
-  case GIO_MEM: {
-    struct gio_mem *gio =  (struct gio_mem*)_gio;
-    ret = vsnprintf(gio->buf + gio->off, gio->sz - gio->off, fmt, args);
-    if(ret >= 0) 
-      gio->off += (ret = GIO_MIN(ret, gio->sz - gio->off));
-    break;
-  }
-  case GIO_FILE: {
-    struct gio_file *gio = (struct gio_file*)_gio;
-
-    if(gio->fp)
-      ret = vfprintf(gio->fp, fmt, args);
-    else
-      // TODO: test _POSIX_SOURCE etc 
-      ret = -1; // vdprintf(gio->fd, fmt, args);
-    break;
-  }
-  case GIO_OPS: {
-    int bufsz;
-    char *buf = _gio_alloc_snprintf(&bufsz, fmt, args);
-    if(buf)
-      ret = gio_write(_gio, buf, bufsz);
-
-    KK_GIO_XALLOC(buf, 0);
     break;
   }
   defualt:
     KK_GIO_UNREACHABLE();
   }
+
+out:
+#ifdef KK_GIO_ENABLE_TRACING
+  do {
+    const char *whence_str = "???";
+    switch(whence) {
+      case SEEK_SET: whence_str = "SEEK_SET"; break;
+      case SEEK_CUR: whence_str = "SEEK_CUR"; break;
+      case SEEK_END: whence_str = "SEEK_END"; break;
+    }
+
+    fprintf(stderr,
+        "%s(): gio=%p { .type=%d }, off=%d, whence=%d(%s) = %d\n", 
+        __func__, _gio, ((struct gio*)_gio)->type, off, whence, whence_str, ret);
+  } while(0);
+  fflush(stderr);
+#endif
   return ret;
 }
 
 KK_GIO_API
-int gio_printf(void *_gio, const char *fmt, ...) 
+off_t gio_pipe(gio_t *src, gio_t *dst) 
+{
+  /* TODO: */
+  KK_GIO_UNREACHABLE();
+}
+
+KK_GIO_API
+int gio_nprintf(gio_t *_gio, size_t maxn, const char *fmt, ...) 
 {
   int ret = -1;
+  void *buf = NULL;
+
   va_list args;
+  int len;
+
   va_start(args, fmt);
-  ret = gio_vprintf(_gio, fmt, args);
+  if((len = vsnprintf(NULL, 0, fmt, args)) < 0)
+    goto out;
   va_end(args);
+
+  /* NOTE: We bypass gio_write */
+  if(((struct gio *)_gio)->type == GIO_MEM) {
+    struct gio_mem *gio = _gio;
+
+    if(_gio_mem_maybe_grow(gio, len + 1))
+      goto out;
+
+    if(gio->gio.flags & GIO_MEM_STRING_AUTOCONTINUE) {
+      if(gio->off && *((char*)gio->buf + gio->off - 1) == '\0') {
+        if(gio_seek(_gio, -1, SEEK_CUR) < 0) {
+          goto out;
+        }
+      }
+    }
+    
+    maxn = GIO_MIN(_gio_mem_left(gio), maxn);
+    va_start(args, fmt);
+    if((len = vsnprintf(gio->buf + gio->off, maxn, fmt, args)) < 0)
+      goto out;
+    va_end(args);
+
+    gio->off += len;
+    gio->off += 1; /* null terminator */
+    KK_GIO_ASSERT(gio->sz >= gio->off);
+
+  } else {
+    maxn = GIO_MIN(len, maxn);
+    if(!(buf = _gio_xalloc(NULL, maxn + 1)))
+      goto out;
+
+    va_start(args, fmt);
+    if((len = vsnprintf(buf, maxn + 1, fmt, args)) < 0)
+      goto out;
+    va_end(args);
+
+    ret = gio_write(_gio, buf, maxn);
+  }
+out:
+  _gio_xalloc(buf, 0);
+#ifdef KK_GIO_ENABLE_TRACING
+  fprintf(stderr,
+    "%s(): gio=%p { .type=%d }, maxn=%zu, fmt=\"%s\", ... = %d\n",
+    __func__, _gio, ((struct gio*)_gio)->type, maxn, fmt, ret);
+  fflush(stderr);
+#endif
   return ret;
 }
 
-KK_GIO_API
-int gio_sync(void *_gio)
-{  
-  switch(((struct gio*)_gio)->type) {
-  case GIO_MEM: {
-    return 0;
-  }
-  case GIO_FILE: {
-    struct gio_file *gio = (struct gio_file*)_gio;
-    if(gio->fp)
-      return fflush(gio->fp);
-    else
-      return fsync(gio->fd);
-  }
-  case GIO_OPS: {
-    struct gio_ops *gio = (struct gio_ops*)_gio;
-    if(gio->sync)
-      return gio->sync(gio);
-    else
-      return 0; /* no sync, no problem */
-  }
-  defualt:
-    KK_GIO_UNREACHABLE();
-  }
-  return -1;
-}
+//{  
+//  int ret = -1;
+//
+//  switch(((struct gio*)_gio)->type) {
+//  case GIO_MEM: {
+//    ret = 0;
+//  }; break;
+//  case GIO_FILE: {
+//    struct gio_file *gio = (struct gio_file*)_gio;
+//    if(gio->fp)
+//      ret = fflush(gio->fp);
+//    else
+//      ret = fsync(gio->fd);
+//  }; break;
+//  case GIO_OPS: {
+//    struct gio_ops *gio = (struct gio_ops*)_gio;
+//    if(gio->sync)
+//      ret =  gio->sync(gio);
+//    else
+//      ret = 0; /* no sync, no problem */
+//  }; break;
+//  defualt:
+//    KK_GIO_UNREACHABLE();
+//  }
+//out:
+//#ifdef KK_GIO_ENABLE_TRACING
+//  fprintf(stderr,
+//    "%s(): gio=%p { .type=%d } = %d\n",
+//    __func__, _gio, ((struct gio*)_gio)->type, ret);
+//  fflush(stderr);
+//#endif
+//  return ret;
+//}
 
-KK_GIO_API
-int gio_close(void *_gio) 
-{
+//{
+//  switch(((struct gio*)_gio)->type) {
+//  case GIO_MEM: {
+//    struct gio_mem *gio = (struct gio_mem*)_gio;
+//    if(gio->gio.flags & GIO_MEM_ALLOC)  {
+//      _gio_xalloc(gio->buf, 0);
+//    }
+//    return 0;
+//  }
+//  case GIO_FILE: {
+//    struct gio_file *gio = (struct gio_file*)_gio;
+//    if(gio->fp)
+//      return fclose(gio->fp);
+//    else
+//      return close(gio->fd);
+//  }
+//  case GIO_OPS: {
+//    struct gio_ops *gio = (struct gio_ops*)_gio;
+//    if(gio->close)
+//      return gio->close(gio);
+//    else
+//      return 0; /* no close, no problem */
+//  }
+//  defualt:
+//    KK_GIO_UNREACHABLE();
+//  }
+//  return -1;
+//}
+
+int gio_ctl(gio_t *_gio, int op, void *user)
+{  
+  int ret = -1;
+
   switch(((struct gio*)_gio)->type) {
   case GIO_MEM: {
-    return 0;
-  }
+    struct gio_mem *gio = (struct gio_mem*)_gio;
+    switch(op) {
+    case GIO_CTL_SYNC:
+      ret = 0; 
+      break;
+
+    case GIO_CTL_CLOSE: 
+      if(gio->gio.flags & GIO_MEM_ALLOC) {
+        _gio_xalloc(gio->buf, 0);
+      }
+      ret = 0; 
+      break;
+    }
+  }; break;
+
   case GIO_FILE: {
     struct gio_file *gio = (struct gio_file*)_gio;
-    if(gio->fp)
-      return fclose(gio->fp);
-    else
-      return close(gio->fd);
-  }
+
+    switch(op) {
+    case GIO_CTL_SYNC:
+      if(gio->fp)
+        ret = fflush(gio->fp);
+      else
+        ret = fsync(gio->fd);
+      break;
+
+    case GIO_CTL_CLOSE: 
+      if(gio->fp)
+        ret = fclose(gio->fp);
+      else
+        ret = close(gio->fd);
+      break;
+    }
+  }; break;
+
   case GIO_OPS: {
     struct gio_ops *gio = (struct gio_ops*)_gio;
-    if(gio->close)
-      return gio->close(gio);
-    else
-      return 0; /* no close, no problem */
-  }
+    if(gio->ctl) {
+      ret = gio->ctl(gio, op, user);
+    } else {
+      if(op < GIO_CTL_USER)
+        ret = 0;
+    }
+  }; break;
+
   defualt:
     KK_GIO_UNREACHABLE();
   }
-  return -1;
+out:
+#ifdef KK_GIO_ENABLE_TRACING
+ do {
+    const char *op_str = "???";
+    switch(op) {
+      case GIO_CTL_SYNC: op_str = "GIO_CTL_SYNC"; break;
+      case GIO_CTL_CLOSE: op_str = "GIO_CTL_CLOSE"; break;
+    }
+
+    fprintf(stderr,
+        "%s(): gio=%p { .type=%d }, op=%d(%s), user=%p = %d\n", 
+        __func__, _gio, ((struct gio*)_gio)->type, op, op_str, user, ret);
+    fflush(stderr);
+  } while(0);
+#endif
+  return ret;
 }
 
 struct gio_list_data {
@@ -610,7 +840,7 @@ int gio_list_add(struct gio_ops *list, struct gio *gio)
   gio_sz = _gio_size(gio);
   KK_GIO_ASSERT(gio_sz > 0);
 
-  new_data = KK_GIO_XALLOC(list->data, 
+  new_data = _gio_xalloc(list->data, 
         sizeof(struct gio_list_data) + data->list_size + gio_sz);
 
   if(new_data) {
@@ -733,7 +963,7 @@ ssize_t gio_list_close_cb(void *_data, const void *buf, size_t sz)
     ret = gio_close(gio, buf, sz);
   }
 #endif
-  KK_GIO_XALLOC(data, 0);
+  _gio_xalloc(data, 0);
   return min;
 }
 
@@ -755,7 +985,7 @@ struct gio_ops gio_list_new(uint8_t flags)
   };
 
 #if 0
-  ops.data = KK_GIO_XALLOC(NULL, sizeof(struct gio_list_data));
+  ops.data = _gio_xalloc(NULL, sizeof(struct gio_list_data));
   if(!ops.data)
     KK_GIO_UNREACHABLE();
 
